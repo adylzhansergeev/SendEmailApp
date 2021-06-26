e8357f1c760b66b22fac84a9fec6a0c769df2e0a
sergeevadylzhan <sergeevadylzhan@gmail.com>
2021-06-26 18:20:11 +0600

    init project

---
+++ src/main/java/com/example/SendEmailApp/SendEmailAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SendEmailApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SendEmailApp/SendEmailAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SendEmailApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
